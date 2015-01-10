7c181ee7cca43457afd9e6d0115eb49b3294d174
Yuwei Ba <xiaobayuwei@gmail.com>
2015-01-11 02:00:48 +0800

    Add prevPoint for calculating radian

--- Your Clock/RootViewController.m
+++ Your Clock/RootViewController.m
@@ -12,14 +12,17 @@
 #define centerX 160
 #define centerY 264
 
-@interface RootViewController ()
+@interface RootViewController () {
+    CGPoint prevPoint;
+}
+
 @end
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    prevPoint = CGPointMake(-1, -1);
     UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveViewWithGestureRecognizerLong:)];
     [self.lineLong addGestureRecognizer:panGestureRecognizer];
 }
@@ -34,27 +37,44 @@
     self.textMinute.text = [@(_minute) stringValue];
 }
 
--(void)moveViewWithGestureRecognizerLong:(UIPanGestureRecognizer *)panGestureRecognizer{
-    
-    CGPoint touchLocation = [panGestureRecognizer locationInView:self.view];
-    
-    self.lineLong.center = touchLocation;
-//    if (panGestureRecognizer.state == UIGestureRecognizerStateChanged) {
-//        self.lineLong.layer.anchorPoint = CGPointMake(0.5, 0);
-//        
-//        CGPoint location = [panGestureRecognizer locationInView:self.lineLong];
-//        
-//        float deltaX = location.x - centerX;
-//        float deltaY = location.y - centerY;
-//        
-//        float angle = atan2f(deltaX, deltaY);
-//        
-//        [self.lineLong setTransform:CGAffineTransformMakeRotation(degree2Radians(angle) * -1)];
-//    }
+-(void) moveViewWithGestureRecognizerLong:(UIPanGestureRecognizer *)panGestureRecognizer
+{
+    if (prevPoint.x == -1) {
+        prevPoint = [panGestureRecognizer locationInView:self.view];
+    }
+    if (panGestureRecognizer.state == UIGestureRecognizerStateChanged) {
+        if (prevPoint.x == -1) // no prev point, do nothing
+            return;
+        
+        self.lineLong.layer.anchorPoint = CGPointMake(0, 1);
+        
+        CGPoint centerPoint = CGPointMake(centerX, centerY);
+        CGPoint touchPoint = [panGestureRecognizer locationInView:self.view];
+        CGFloat angle = angleBetweenLinesInDegrees(centerPoint, prevPoint, centerPoint, touchPoint);
+        
+        self.lineLong.layer.transform = CATransform3DMakeRotation(angle, 0, 0, 1);
+    }
     
 }
 
 float degree2Radians(float degrees) { return degrees * M_PI / 180; }
+
+CGFloat angleBetweenLinesInDegrees(CGPoint beginLineA,
+                                   CGPoint endLineA,
+                                   CGPoint beginLineB,
+                                   CGPoint endLineB)
+{
+    CGFloat a = endLineA.x - beginLineA.x;
+    CGFloat b = endLineA.y - beginLineA.y;
+    CGFloat c = endLineB.x - beginLineB.x;
+    CGFloat d = endLineB.y - beginLineB.y;
+    
+    CGFloat atanA = atan2(a, b);
+    CGFloat atanB = atan2(c, d);
+    
+    // convert radiants to degrees
+    return (atanA - atanB) * 180 / M_PI;
+}
 
 - (void) viewWillAppear:(BOOL)animated
 {
